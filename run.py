f73e16ea28ce971fd0d81dad095eed2ec22d94c6
sarahmenks <sarahmenksperber@gmail.com>
2026-06-15 11:26:41 -0300

    Add: send_emails() try catch and failures feedback

--- run.py
+++ run.py
@@ -2,25 +2,41 @@ import sys
 import smtplib
 from email.mime.text import MIMEText
 
-def send_email(destination: str, name: str) -> bool:
+def send_email(name: str, destination: str) -> bool:
     """
     Envia email com o arquivo PDF anexado.
     Retorna True se bem-sucedido, False caso contrário.
     """
+    try:
+        msg = MIMEText(f"Olá, {name}!")
+        msg['Subject'] = "Teste de Email"
+        msg['To'] = destination
+        msg['From'] = "exemplo@gmail.com"
 
-    msg = MIMEText(f"Olá, {name}!")
-    msg['Subject'] = "Teste de Email"
-    msg['To'] = destination
-    msg['From'] = "exemplo@gmail.com"
+        print(msg.as_string())  # Apenas para demonstração, não envia o email
+        # TODO: Configurar SMTP, fazer opção de anexar PDF e enviar email
 
-    # TODO: Configurar SMTP, fazer opção de anexar PDF e enviar email
-
-    return True
+        return True
+    
+    except Exception as e:
+        print(f"Erro ao enviar email para {name} ({destination}): {e}")
+    
+    return False
 
 def main():
-    destination = "destination@gmail.com"
-    name = "João"
-    send_email(destination, name)
+    emails = [("João", "destination@gmail.com"), ("",7679)]
+    failures = []
+    
+    for name, destination in emails:
+        if send_email(name, destination):
+            print(f"Email enviado para {name} ({destination})")
+        else:
+            print(f"Falha ao enviar email para {name} ({destination})")
+            failures.append((name, destination))
+
+    if failures:
+        print("Total de falhas:", len(failures))
+        print("Falhas:", failures)
 
 
 if __name__ == "__main__":

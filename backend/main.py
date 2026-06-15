6b3df9e2f6315e1b63be90c79d52638a508505ce
Ari <arifilhosilva235@gmail.com>
2026-06-15 11:22:15 -0300

    feat: cria classe de doador

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,45 @@
-from fastapi import FastAPI
+from typing import List, Optional
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-app = FastAPI()
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+class Doador(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nome: str
+    email: str
+    valor: float
+
+
+app = FastAPI(title="Gerenciador de Doações")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 
 @app.get("/")
 def read_root():

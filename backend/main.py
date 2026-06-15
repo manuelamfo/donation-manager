5213c8e8fb74ad47c70fe72a9230b03c707395b2
Ari <arifilhosilva235@gmail.com>
2026-06-15 11:27:44 -0300

    fix: separa doador da doacao

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,30 @@
 from typing import List, Optional
+from datetime import datetime
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Relationship
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
-
 class Doador(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     nome: str
     email: str
+    
+    doacoes: List["Doacao"] = Relationship(back_populates="doador")
+
+
+class Doacao(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
     valor: float
+    data: datetime = Field(default_factory=datetime.now)
+    
+    doador_id: int = Field(foreign_key="doador.id")
+    
+    doador: Doador = Relationship(back_populates="doacoes")
 
 
 app = FastAPI(title="Gerenciador de Doações")

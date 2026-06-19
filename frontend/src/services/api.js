e095d4fa5f54892d4404b72367bc6721e5d88b5c
Ari <arifilhosilva235@gmail.com>
2026-06-19 16:43:13 -0300

    feat: place the authorization token in the request

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,7 +7,29 @@ export const api = axios.create({
     },
 });
 
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
 export const loginUser = async (email, password) => {
     const response = await api.post('/auth/login', { email, password });
     return response.data;
+};
+
+export const getDonations = async () => {
+    const response = await api.get('/donations/');
+    return response.data;
+};
+
+export const createDonation = async (donationData) => {
+    const response = await api.post('/donations/', donationData);
+    return response.data;
+};
+
+export const deleteDonation = async (id) => {
+    await api.delete(`/donations/${id}`);
 };

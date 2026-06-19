ceeac66b893770c214acba95046c4385264e8758
Ari <arifilhosilva235@gmail.com>
2026-06-19 08:35:29 -0300

    fix: remove user model

--- backend/models.py
+++ backend/models.py
@@ -2,13 +2,6 @@ from typing import List, Optional
 from datetime import datetime
 from sqlmodel import Field, SQLModel, Relationship
 
-class User(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    name: str
-    email: str = Field(unique=True, index=True)
-    password: str
-    active: bool = Field(default=True)
-
 class Donor(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str
--- backend/routers/users.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/users", tags=["Users"])
-
-@router.get("/")
-def read_root():
-    return {"message": "Hello, world!"}

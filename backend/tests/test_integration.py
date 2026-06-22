f1365d949c71d791eba358b0396c742e953da3d2
manuelamfo <manuelamfo@gmail.com>
2026-06-22 19:41:48 -0300

    test: teste de integração em que o envio de email falha

--- backend/tests/test_integration.py
+++ backend/tests/test_integration.py
@@ -8,7 +8,7 @@ client = TestClient(app)
 
 # ---- Testes de integração para verificar o envio de emails ---
 
-def test_spy_send_email_to_donors_sucess(mocker):
+def test_spy_send_email_to_donors_both_success(mocker):
     
     # faz o mock das configurações para garantir que o SMTP conste como configurado
     mocker.patch.object(type(donations.settings), "smtp_configurado", return_value = True)
@@ -16,7 +16,7 @@ def test_spy_send_email_to_donors_sucess(mocker):
     # faz o mock de smtplib para não enviar e-mails reais e não quebrar o teste
     mock_smtp = mocker.patch("backend.routers.donations.smtplib.SMTP")
 
-    # cria o spy na função interna _enviar_emai
+    # cria o spy na função interna _enviar_email
     spy_enviar_email = mocker.spy(donations, "_enviar_email")
 
     payload = {
@@ -38,7 +38,7 @@ def test_spy_send_email_to_donors_sucess(mocker):
     spy_enviar_email.assert_any_call("doador2@gmail.com", "Obrigado!", "Sua doação ajudou muito!")
 
 
-def test_spy_send_email_to_donor_fail(mocker):
+def test_spy_send_email_to_donors_one_fail(mocker):
     
     # faz o mock das configurações para garantir que o SMTP conste como configurado
     mocker.patch.object(type(donations.settings), "smtp_configurado", return_value = True)
@@ -46,23 +46,36 @@ def test_spy_send_email_to_donor_fail(mocker):
     # faz o mock de smtplib para não enviar e-mails reais e não quebrar o teste
     mock_smtp = mocker.patch("backend.routers.donations.smtplib.SMTP")
 
-    # cria o spy na função interna _enviar_emai
+    # cria o spy na função interna _enviar_email
     spy_enviar_email = mocker.spy(donations, "_enviar_email")
+
+    def erro_spy_doador(destinatario, subject, body):
+        if destinatario == "doador1@gmail.com":
+            raise Exception("Timeout na conexão")
+        return None
+
+    # injetando o erro no spy como side effect, para testar quando o envio de e-mail da errado
+    spy_enviar_email.side_effect = erro_spy_doador
 
     payload = {
         "emails": ["doador1@gmail.com", "doador2@gmail.com"],
-        "subject": "Obrigado!",
-        "body": "Sua doação ajudou muito!"
+        "subject": "Gratidão!",
+        "body": "Sua doação nos ajuda a continuarmos!"
     }
 
     response = client.post("/donations/send-email", json=payload)
 
     # validação da resposta da API
     assert response.status_code == status.HTTP_200_OK
-    assert response.json()["message"] == "E-mail enviado com sucesso para 2 contato(s)!"
+    dados = response.json()
 
     # validação do spy
-    assert spy_enviar_email.call_count == 2
 
-    spy_enviar_email.assert_any_call("doador1@gmail.com", "Obrigado!", "Sua doação ajudou muito!")
-    spy_enviar_email.assert_any_call("doador2@gmail.com", "Obrigado!", "Sua doação ajudou muito!")
+    assert spy_enviar_email.call_count == 2
+    assert "1 falha(s)" in dados["message"]
+    assert dados["failures"][0]["email"] == "doador1@gmail.com"
+    assert dados["failures"][0]["erro"] == "Timeout na conexão"
+
+    assert "E-mail enviado para 1 de 2 contato(s)." in dados["message"]
+
+    spy_enviar_email.assert_any_call("doador2@gmail.com", "Gratidão!",  "Sua doação nos ajuda a continuarmos!")

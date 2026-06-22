57934495e29d79acd872de307851121a0d844d35
manuelamfo <manuelamfo@gmail.com>
2026-06-22 20:34:23 -0300

    feat: arquivo de fixture para realizar os testes que utilizam o banco de dados

---
+++ backend/tests/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel.pool import StaticPool
+
+from ..main import app
+from ..database import get_session
+
+@pytest.fixture(name="engine", scope="session")
+def engine_fixture():
+    engine = create_engine(
+        "sqlite://",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
+    SQLModel.metadata.create_all(engine)
+    yield engine
+    SQLModel.metadata.drop_all(engine)
+
+@pytest.fixture(name="session")
+def session_fixture(engine):
+    with Session(engine) as session:
+        yield session
+        session.rollback()
+
+@pytest.fixture(name="client")
+def client_fixture(session):
+    def override_get_session():
+        yield session
+
+    app.dependency_overrides[get_session] = override_get_session
+    yield TestClient(app)
+    app.dependency_overrides.clear()
